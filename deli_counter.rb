405ea94c8c01e847eb5d2e92359bcee5ef95ead2
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-09 23:06:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     string = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-    string += (" #{position}. " + name)
+    katz_deli.each_with_index do |index, name|
+    string += (" #{index}. " + name)
     end
     puts string
     end
