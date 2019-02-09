5574be86dc0097ed758b3b57c0dcebaa818fd3bc
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-09 23:07:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   else
     string = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-    string += (" #{index.to_s}. " + name)
+    string += (" #{index}. " + name)
     end
     puts string
     end
